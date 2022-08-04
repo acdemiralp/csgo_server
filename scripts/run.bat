352dd971c2da44cdc440a6ee1da5aab864ee993f
acdemiralp <demiralpali@gmail.com>
2022-08-04 15:17:39 +0200

    Everything works.

--- scripts/run.bat
+++ scripts/run.bat
@@ -8,21 +8,26 @@ for /l %%n in () do (
     call install_server.bat
     call configure.bat
   )
+  
+  cd ..\server
 
-  set BASE_PARAMETERS=-game csgo -console -usercon +game_type %GAME_TYPE% +game_mode %GAME_MODE%
-  set GSLT_PARAMETERS=+sv_setsteamaccount %GSLT% -net_port_try 1
-  set MAP_PARAMETERS=+mapgroup %MAP_GROUP%
-  if defined (%DEFAULT_MAP%) (
-    set MAP_PARAMETERS=%MAP_PARAMETERS% +map %DEFAULT_MAP%
+  set "BASE_PARAMETERS=-game csgo -console -usercon +game_type %GAME_TYPE% +game_mode %GAME_MODE%"
+  set "GSLT_PARAMETERS=+sv_setsteamaccount %GSLT% -net_port_try 1"
+  set "MAP_PARAMETERS=+mapgroup %MAP_GROUP%"
+
+  if not [%DEFAULT_MAP%] == [] (
+    set "MAP_PARAMETERS=!MAP_PARAMETERS! +map %DEFAULT_MAP%"
   )
-  if defined (%WORKSHOP_MAP_COLLECTION_ID%) (
-    set MAP_PARAMETERS=+host_workshop_collection %WORKSHOP_MAP_COLLECTION_ID% -authkey %STEAM_WEB_API_KEY%
-    if defined (%WORKSHOP_DEFAULT_MAP_ID%) (
-      set MAP_PARAMETERS=%MAP_PARAMETERS% +workshop_start_map %WORKSHOP_DEFAULT_MAP_ID%
+  if not [%WORKSHOP_MAP_COLLECTION_ID%] == [] (
+    set "MAP_PARAMETERS=+host_workshop_collection %WORKSHOP_MAP_COLLECTION_ID% -authkey %STEAM_WEB_API_KEY%"
+    if not [%WORKSHOP_DEFAULT_MAP_ID%] == [] (
+      set "MAP_PARAMETERS=!MAP_PARAMETERS! +workshop_start_map %WORKSHOP_DEFAULT_MAP_ID%"
     )
   )
 
-  cd ..\server
-  srcds %BASE_PARAMETERS% %GSLT_PARAMETERS% %MAP_PARAMETERS%
+  set "COMMAND=srcds !BASE_PARAMETERS! !GSLT_PARAMETERS! !MAP_PARAMETERS!"
+  echo COMMAND is: !COMMAND!
+  !COMMAND!
+
   cd ..\scripts
 )
